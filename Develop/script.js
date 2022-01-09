d6416758f2fb688b2979528a19a6bc75c344fe44
haybales <haleynicole126@gmail.com>
2022-01-09 16:28:48 -0700

    initial prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,59 @@
 // Assignment code here
 
 
+var passwordLength = function() {
+  var promptLength = parseInt(window.prompt("Please enter desired password length between 8 and 128 characters."))
+  if (isNaN(promptLength) || promptLength < 8 || promptLength > 128) {
+    window.alert("Invalid Response");
+    return passwordLength();
+  } else {
+    window.alert("You have a requested a password " + promptLength + " characters long.");
+    return promptLength;
+  }
+};
+
+
+var passwordLower = function() {
+  var promptLower = window.confirm("Do you want lowercase letters in your password?")
+  return promptLower;
+
+};
+
+var passwordUpper = function() {
+  var promptUpper = window.confirm("Do you want uppercase letters in your password?")
+  return promptUpper;
+
+};
+
+var passwordNumber = function() {
+  var promptNum = window.confirm("Do you want numeric characters in your password?")
+  return promptNum;
+
+};
+
+var passwordSpecial = function() {
+  var promptSpecial = window.confirm("Do you want special characters in your password?")
+  return promptSpecial;
+
+};
+
+var passInfo = {
+  length: passwordLength(),
+  lower: passwordLower(),
+  upper: passwordUpper(),
+  num:  passwordNumber(),
+  special: passwordSpecial(),
+};
+
+// Is at least one character type selected? 
+
+
+
+// password generated that matches selected criteria
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +68,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
